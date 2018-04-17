b3f0cba8d51fb5dcf36257841c68d107d6e800e1
Your Namehglottman <hglottman@gmail.com>
2018-04-17 18:00:48 +0300

    with EXTENSION 1

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ var beers = [];
 var addBeer = function () {
     var nameOfBeer = $('.beer-input').val();
     var nameOfCategory = $('.category-input').val();
+    var beerScore = $('.chosenBeers').val();
     var beer = {
-name: nameOfBeer,
-category: nameOfCategory
+        name: nameOfBeer,
+        category: nameOfCategory,
+        score: beerScore
     }
-     beers.push(beer); 
+    beers.push(beer);
 };
 
 
-var renderBeers = function(){
+var renderBeers = function () {
     $('.beers-list').find('li').remove();
-    for(let i = 0; i < beers.length; i++){
-        $('.beers-list').append('<li>' + beers[i].name + '</li>'); 
-    }
-
+    for (let i = 0; i < beers.length; i++) {
+        $('.beers-list').append('<li>' + beers[i].name + " " + beers[i].score + '</li>');
+    };
 };
 
 
 
 $('.post-beer').click(addBeer);
 $('.render-beer').click(renderBeers);
+
